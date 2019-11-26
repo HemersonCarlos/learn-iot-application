2b3b3a44c6b540fc4b574193b15d002e7094f032
HemersonCarlos <hemersoncarlos93@hotmail.com>
2019-11-26 19:17:41 -0200

    Configurado o Index ( Botão Home )

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Plataforma IoT', sensors: sensors, actuators: actuators } );
 });
 
+router.post('/:id', function(req, res, next) {
+
+  var actuators = devicesService.getActuators();
+  var id = req.params.id;
+  var actuator = actuators.find(actuator => actuator.id == id);
+
+  actuator.status = actuator.status ? 0 : 1;
+
+  devicesService.saveFileActuators(actuators);
+
+  res.redirect('/');
+
+});
+
 module.exports = router;
